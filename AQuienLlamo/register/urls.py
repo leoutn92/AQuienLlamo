557495136cbc9c7e966884bbd1d3f222da3fe468
Makenrro <Makenrro@users.noreply.github.com>
2015-06-22 15:46:13 -0300

    Update urls.py

--- AQuienLlamo/register/urls.py
+++ AQuienLlamo/register/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from .views import  RegServicio
 urlpatterns = [
     url(r'^$','register.views.register'),
     url(r'/cli','register.views.register_cli'),
-    url(r'/prov','register.views.register_prov')
+    url(r'/prov','register.views.register_prov'),
+    url(r'^regservicio/$' , RegServicio.as_view()), 
 ]
