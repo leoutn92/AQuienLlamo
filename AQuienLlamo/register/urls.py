6b3a62c8c669ca3160082ff09982e84c776a777d
leoutn92 <leoutn92@gmail.com>
2015-10-05 14:43:40 -0300

    Sacando la basura

--- AQuienLlamo/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from register.views import RegServicio
-urlpatterns = [
-    url(r'^$','register.views.register'),
-    url(r'/cli','register.views.register_cli'),
-    url(r'/prov$','register.views.register_prov'),
-    url(r'/prov/regServicio$' , RegServicio.as_view()), 
-]
--- AQuienLlamo/welcome/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-from register.models import Usuario,Cliente,Proveedor
-def welcome(request):
-	return render(request,'auth.html')
